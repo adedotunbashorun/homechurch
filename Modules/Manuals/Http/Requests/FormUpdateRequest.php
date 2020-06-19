c3eadca94bcd2c04e9d25f8b19188bbe25ff7b86
adedotunbashorun <adedotunolawale@gmail.com>
2020-06-19 10:49:56 +0100

    remove dd

---
+++ Modules/Manuals/Http/Requests/FormUpdateRequest.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Modules\Manuals\Http\Requests;
+
+use Modules\Core\Http\Requests\AbstractFormRequest;
+
+class FormUpdateRequest extends AbstractFormRequest
+{
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'name' => 'required|min:5',
+            'body' => 'required|min:5',
+            // 'document' => 'required|mimes:doc,docx,jpg,jpeg,pdf,png,txt|max:2048'
+        ];
+    }
+
+
+}
